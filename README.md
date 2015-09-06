12ab67b17134d7d17cdf883490e31144438ab166
ChrisQ <chris@kotelett.no>
2015-09-06 17:12:30 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# lt-tools
+
+''Usage:''
+
+Set range on all G25/27 wheels to 200 (for Dirt Showdown):
+
+```
+sudo lt-range.py 200
+```
---
+++ lt-range.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python2
+
+from os import walk
+import sys
+from re import match
+
+if len(sys.argv) < 2:
+    sys.exit('Need range parameter, 200-900')
+
+if not 199 < int(sys.argv[1]) < 901:
+    sys.exit('Out of range, 200-900')
+
+fsroot = '/sys/bus/hid/drivers/logitech'
+ltroot = walk(fsroot).next()
+
+for ddir in ltroot[1]:
+    if match(r'\d{4}:.{4}:.{4}\.\d{4}$', ddir):
+        devfiles = walk(fsroot + '/' + ddir).next()
+        if 'real_id' and 'range' in devfiles[2]:
+            with open(fsroot + '/' + ddir + '/' + 'real_id', 'r') as dev_id:
+                if 'G27' or 'G25' in dev_id.readline():
+                    # print "setting range %s" % sys.argv[1]
+                    with open(fsroot + '/' + ddir + '/' + 'range', 'w') as dev_range:
+                        dev_range.write(sys.argv[1])
+
