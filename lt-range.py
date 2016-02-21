a6c666ef5abdd97b6957a4848902267f8756d64b
ChrisQ <chris@kotelett.no>
2016-02-21 19:34:13 +0100

    Now works with python 3 as well.

--- lt-range.py
+++ lt-range.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
+from __future__ import print_function
 
 from os import walk
 import sys
@@ -11,15 +12,18 @@ if not 199 < int(sys.argv[1]) < 901:
     sys.exit('Out of range, 200-900')
 
 fsroot = '/sys/bus/hid/drivers/logitech'
-ltroot = walk(fsroot).next()
+try:
+    ltroot = next(walk(fsroot))
+except StopIteration as e:
+    sys.exit('{} {}'.format('No logitech devices found in', fsroot))
 
 for ddir in ltroot[1]:
     if match(r'\d{4}:.{4}:.{4}\.\d{4}$', ddir):
-        devfiles = walk(fsroot + '/' + ddir).next()
+        devfiles = next(walk(fsroot + '/' + ddir))
         if 'real_id' and 'range' in devfiles[2]:
             with open(fsroot + '/' + ddir + '/' + 'real_id', 'r') as dev_id:
                 if 'G27' or 'G25' in dev_id.readline():
-                    # print "setting range %s" % sys.argv[1]
+                    # print("setting range %s" % sys.argv[1])
                     with open(fsroot + '/' + ddir + '/' + 'range', 'w') as dev_range:
                         dev_range.write(sys.argv[1])
 
